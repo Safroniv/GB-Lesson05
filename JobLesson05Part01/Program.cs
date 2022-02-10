a6b4f32b637b89b494e7b9337ed013200f8a9db3
Safroniv <safroniv@mail.ru>
2022-02-10 21:06:07 +0300

    Урок 5 Задание 1

--- JobLesson05Part01/Program.cs
+++ JobLesson05Part01/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.IO;
 
 namespace JobLesson05Part01
 {
     internal class Program
     {
+        //Урок 5 Задание 1
+        //Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
 
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Здравствуйте, введите данные для сохранения в файл:");
+            string inputData = Console.ReadLine();
+            string inputInFile = "text.txt";
+            File.WriteAllText(inputInFile, inputData);
         }
     }
 }
