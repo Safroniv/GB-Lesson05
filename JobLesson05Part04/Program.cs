94515e6aabf51220037b27bd4da26e9f924c2eae
Safroniv <safroniv@mail.ru>
2022-02-12 15:18:12 +0300

    Создание ветви Урок 5 Задание 4

---
+++ JobLesson05Part04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobLesson05Part04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
