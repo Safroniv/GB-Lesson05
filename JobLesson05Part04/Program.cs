1c8bf5573936e62eb095327f8a93e089166f3ced
Safroniv <safroniv@mail.ru>
2022-02-19 22:41:39 +0300

    Урок 5 Задание 4 - в процессе3

--- JobLesson05Part04/Program.cs
+++ JobLesson05Part04/Program.cs
@@ -1,6 +1,4 @@
-﻿using ICSharpCode.Decompiler.CSharp.Syntax.PatternMatching;
-using System;
-using System.Collections;
+﻿using System;
 using System.IO;
 
 namespace JobLesson05Part04
@@ -42,8 +40,14 @@ namespace JobLesson05Part04
 
                     if (Directory.Exists(dirs[i]))
                     {
+                        string[] subDirs = Directory.GetDirectories(dirs[i]);
+                        for (int j = 0; j < subDirs.Length; j++)
+                        {
+                            Console.WriteLine("Категория:" + subDirs[j]);
+                            File.AppendAllText("Structure.txt", Environment.NewLine + "Категория:" + subDirs[j]);
 
-                    }                   
+                        }
+                    }
                 }
                 string[] files = Directory.GetFiles(structDirName);
                 for (int i = 0; i < files.Length; i++)
@@ -57,60 +61,60 @@ namespace JobLesson05Part04
         }
     }
 }
-            //Формирование структуры для вложений до 3-его уровня включительно
-            //static string TreeOfCategory()
-            //{
-            //    string structDirName = @"F:\CategoryForTree";
-            //    Console.WriteLine("Структура:");
-            //    File.AppendAllText("Structure.txt", Environment.NewLine + "Подкаталоги:");
-            //    string[] dirs = Directory.GetDirectories(structDirName);
-            //    foreach (string s in dirs)
-            //    {
-            //        Console.WriteLine("Категория:" + s);
-            //        File.AppendAllText("Structure.txt", Environment.NewLine + "Категория:" + s);
-            //        string[] dirs2 = Directory.GetDirectories(s);
-            //        foreach (string t in dirs2)
-            //        {
-            //            Console.WriteLine("Подкатегория:" + t);
-            //            File.AppendAllText("Structure.txt", Environment.NewLine + "Подкатегория:" + t);
-            //            string[] dirs3 = Directory.GetDirectories(t);
-            //            foreach (string x in dirs3)
-            //            {
-            //                Console.WriteLine("Подподкатегория:" + x);
-            //                File.AppendAllText("Structure.txt", Environment.NewLine + "Подподкатегория:" + x);
-            //                string[] files3 = Directory.GetFiles(x);
-            //                foreach (string y in files3)
-            //                {
-            //                    Console.WriteLine("Файл подподкатегории:" + y);
-            //                    File.AppendAllText("Structure.txt", Environment.NewLine + "Файл подподкатегории:" + y);
-            //                }
-            //            }
-            //        }
-            //        string[] files2 = Directory.GetFiles(s);
-            //        foreach (string u in files2)
-            //        {
-            //            Console.WriteLine("Файл подкатегории:" + u);
-            //            File.AppendAllText("Structure.txt", Environment.NewLine + "Файл подкатегории:" + u);
-            //        }
-            //    }
-            //    Console.WriteLine();
-            //    string[] files = Directory.GetFiles(structDirName);
-            //    foreach (string s in files)
-            //    {
-            //        Console.WriteLine("Файл категории:" + s);
-            //        File.AppendAllText("Structure.txt", Environment.NewLine + "Файл категории:" + s);
-            //    }
-            //    return structDirName;
-            //}
-            //Получение файлов и категорий 1-ого уровня
-            //static void Main()
-            //{
-            //    string dirName = null;
-            //    string name = TreeOfCategory(dirName);
-            //    Console.WriteLine(name);
-            //}
-            //static string TreeOfCategory(string dirName)
-            //{
+//Формирование структуры для вложений до 3-его уровня включительно
+//static string TreeOfCategory()
+//{
+//    string structDirName = @"F:\CategoryForTree";
+//    Console.WriteLine("Структура:");
+//    File.AppendAllText("Structure.txt", Environment.NewLine + "Подкаталоги:");
+//    string[] dirs = Directory.GetDirectories(structDirName);
+//    foreach (string s in dirs)
+//    {
+//        Console.WriteLine("Категория:" + s);
+//        File.AppendAllText("Structure.txt", Environment.NewLine + "Категория:" + s);
+//        string[] dirs2 = Directory.GetDirectories(s);
+//        foreach (string t in dirs2)
+//        {
+//            Console.WriteLine("Подкатегория:" + t);
+//            File.AppendAllText("Structure.txt", Environment.NewLine + "Подкатегория:" + t);
+//            string[] dirs3 = Directory.GetDirectories(t);
+//            foreach (string x in dirs3)
+//            {
+//                Console.WriteLine("Подподкатегория:" + x);
+//                File.AppendAllText("Structure.txt", Environment.NewLine + "Подподкатегория:" + x);
+//                string[] files3 = Directory.GetFiles(x);
+//                foreach (string y in files3)
+//                {
+//                    Console.WriteLine("Файл подподкатегории:" + y);
+//                    File.AppendAllText("Structure.txt", Environment.NewLine + "Файл подподкатегории:" + y);
+//                }
+//            }
+//        }
+//        string[] files2 = Directory.GetFiles(s);
+//        foreach (string u in files2)
+//        {
+//            Console.WriteLine("Файл подкатегории:" + u);
+//            File.AppendAllText("Structure.txt", Environment.NewLine + "Файл подкатегории:" + u);
+//        }
+//    }
+//    Console.WriteLine();
+//    string[] files = Directory.GetFiles(structDirName);
+//    foreach (string s in files)
+//    {
+//        Console.WriteLine("Файл категории:" + s);
+//        File.AppendAllText("Structure.txt", Environment.NewLine + "Файл категории:" + s);
+//    }
+//    return structDirName;
+//}
+//Получение файлов и категорий 1-ого уровня
+//static void Main()
+//{
+//    string dirName = null;
+//    string name = TreeOfCategory(dirName);
+//    Console.WriteLine(name);
+//}
+//static string TreeOfCategory(string dirName)
+//{
 
 //    string structDirName = @"F:\CategoryForTree";
 
