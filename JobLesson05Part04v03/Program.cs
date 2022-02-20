f66fd1f99e38eddd86c2cf37e716c3e296c77639
Safroniv <safroniv@mail.ru>
2022-02-20 13:07:44 +0300

    Урок 5 задание 4 вар 3 - через foreach до 3его уровня

---
+++ JobLesson05Part04v03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobLesson05Part04v03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
