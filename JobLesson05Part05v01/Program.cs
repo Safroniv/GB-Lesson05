f883f835c0a1fb451df877b5ae2d066e4dc3ec60
Safroniv <safroniv@mail.ru>
2022-02-20 17:47:57 +0300

    Урок 5 Задание 5 вар01

---
+++ JobLesson05Part05v01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobLesson05Part05v01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
